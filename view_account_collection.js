122ba630d12d1690dc8503e7042e0f646006d70e
resilience_me <emailsareobsolete@gmail.com>
2015-01-06 13:47:03 +0000

    a

--- view_account_collection.js
+++ view_account_collection.js
@@ -4,7 +4,7 @@ var db = mongojs("mongodb://guest:guest@ds049150.mongolab.com:49150/basicincome_
 var ACCOUNT = "undefinedrBTK58wwB5yGKN9gj9gUbxicZxPgHMJGgH"
 
 
- db.collection(ACCOUNT).find({  },function (err, doc){
+ db.collection(ACCOUNT).find({ type:"tax_blob" },function (err, doc){
         
     
         console.log(doc)
